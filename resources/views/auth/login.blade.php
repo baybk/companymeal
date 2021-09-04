3f43954540171b759bced6ec7d44a8c53b61c227
bayna4k13 <bayna4k13@gmail.com>
2021-09-04 18:18:58 +0700

    remove-readonly

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('messages.email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ session('toEmail') }}" required autocomplete="email" readonly autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ session('toEmail') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
