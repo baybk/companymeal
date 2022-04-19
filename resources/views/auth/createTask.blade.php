c241ea9e5588daf4d06763fce4e0d6ca1e075828
bayna4k13 <bayna4k13@gmail.com>
2022-04-20 01:47:30 +0700

    fix-task-type

--- resources/views/auth/createTask.blade.php
+++ resources/views/auth/createTask.blade.php
@@ -35,7 +35,7 @@
                                     <option value="BA">BA</option>
                                     <option value="Database">Database</option>
                                     <option value="Testing">Testing</option>
-                                    <option value="Testing">General</option>
+                                    <option value="General">General</option>
                                 </select>
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
